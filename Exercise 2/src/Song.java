52beabadc8dad0ca38f68d1eb2884793fd5e1550
xANGEL143 <143593111+xANGEL143@users.noreply.github.com>
2023-09-04 12:48:43 +0000

    Add files via upload

---
+++ Exercise 2/src/Song.java
@@ -0,0 +1,11 @@
+public class Song {
+    String name;
+    String Artistname; 
+    String album; 
+
+    public Song(String name, String Artistname, String album){
+        this.name = name; 
+        this.Artistname = Artistname; 
+        this.album = album;
+    }
+}
