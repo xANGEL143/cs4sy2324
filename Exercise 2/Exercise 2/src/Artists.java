75abcca0429ed914d94d1c6ebc0500ee29cbe1b0
xANGEL143 <143593111+xANGEL143@users.noreply.github.com>
2023-09-11 23:28:00 +0000

    Add files via upload

---
+++ Exercise 2/Exercise 2/src/Artists.java
@@ -0,0 +1,12 @@
+class Artists {
+    String name; 
+    int age; 
+    String band; 
+
+    //constructor
+    public Artists(String name, int age, String band){
+        this.name = name; 
+        this.age = age; 
+        this.band = band; 
+    }
+}
